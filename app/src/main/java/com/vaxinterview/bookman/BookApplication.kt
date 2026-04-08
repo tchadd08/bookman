1fd2b7a4cd052a001e13fe708afada85c176a1af
Chadd Van Komen <chaddvankomen@gmail.com>
2026-04-08 12:16:45 -0600

    Initial commit, mostly dependencies

---
+++ app/src/main/java/com/vaxinterview/bookman/BookApplication.kt
@@ -0,0 +1,9 @@
+package com.vaxinterview.bookman
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookApplication : Application() {
+    //empty extension of application for hilt usage
+}
